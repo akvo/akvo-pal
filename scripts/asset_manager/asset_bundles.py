4b0e5f6978fc4dff09a8b86dc44d34f894b019d4
Daniel Karlsson <daniel@fraction4.com>
2010-10-19 17:15:56 +0200

    Added the scripts folder

---
+++ scripts/asset_manager/asset_bundles.py
@@ -0,0 +1,18 @@
+
+ASSET_BUNDLES = {
+    'akvo_style': {
+        'type': 'css',
+        'path': 'css/',
+        'compress': True,
+        'files': [
+            'reset.css',
+            'grid.css',
+            'typography.css',
+            'base.css',
+            'utils.css',
+            'main_nav.css',
+            'index.css',
+            'buttons.css',
+            ],
+    },
+}
