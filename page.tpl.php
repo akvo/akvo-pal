317d79b0bde93122a5565a1be9ab4267530b9ca8
Daniel Karlsson <daniel@fraction4.com>
2010-10-26 13:56:54 +0200

    Fixed issues in the new footer content.

--- page.tpl.php
+++ page.tpl.php
@@ -65,16 +65,7 @@
 <div id="footer">
 	<div id="footer_container" class="container">
 		<div class="span-18">
-			<h2>About Akvo.org</h2>
-			<p>
-                We are a non-profit foundation headquartered in the Netherlands. We help connect 
-                doers and donors to transform communities in some of the poorest parts of the world. 
-                The Akvo platform is open source and we encourage others to build on and improve 
-                these tools, or adapt the platform for other purposes.
-			</p>
-		</div>
-		<div class="span-18">
-		    <div class="span-3 first">
+			<div class="span-3 first">
 				<h3>Policies</h3>
 				<p class="small">
 					<a href="/web/terms_of_use">General terms of use</a><br />
@@ -82,9 +73,9 @@
 					<a href="/web/donation_policy">Donations</a><br />
 					<a href="/web/partners_and_projects_policy">Partners</a><br />
 					<a href="/web/akvo-rsr-api-code-of-conduct">
-						Akvo RSR API Code of Conduct
+						Akvo API code of conduct
 					</a><br />
-					<a href="/web/user_contribution_policy">User Contribution Policy</a><br />	
+					<a href="/web/user_contribution_policy">User submitted content</a><br />	
 					<a href="/web/open_license">Licensing and copyrights</a><br />
 				</p>
 			</div>
@@ -95,7 +86,7 @@
 					<a href="/web/how-it-works">How it works</a><br />
 					<a href="/web/team">Team</a><br />
 					<a href="/web/jobs">Working at Akvo</a><br />
-					<a href="/web/faq">Akvo Frequently Asked Questions</a><br />
+					<a href="/web/faq">Akvo FAQ</a><br />
 					<br />
 					<a href="/web/press">Press</a><br />
 					<a href="/web/annual_reports">Annual reports</a><br />
@@ -115,25 +106,25 @@
 			<div class="span-3">
 				<h3>Developers</h3>
 				<p class="small">
-					<a href="/rsr/admin">Akvo RSR login</a><br />
-					<a href="/blog/wp-login.php">Blog</a><br />
-					<a href="/web/?q=node/24">CMS</a><br />
+					<a href="/labs/">AkvoLabs</a><br />
+					<a href="http://github.com/akvo/">Source code</a><br />
 				</p>
 			</div>
 			<div class="span-3">
 				<h3>Admin</h3>
 				<p class="small">
-					<a href="/labs/">AkvoLabs</a><br />
-					<a href="http://github.com/akvo/">Source Code</a><br />
+					<a href="/rsr/admin">Akvo RSR login</a><br />
+					<a href="/blog/wp-login.php">Blog</a><br />
+					<a href="/web/?q=node/24">CMS</a><br />
 				</p>
 			</div>
 			<div class="span-3 last">
 				<h3>Contact us</h3>
 				<p class="small">
-					<a href="http://help.akvo.org/">Help + Support</a><br />
+					<a href="http://help.akvo.org/">Help + support</a><br />
 					<a href="/web/contact_us">Contact info</a><br />
 					<br />
-					<a class="red" href="http://help.akvo.org/discussion/new ">Report a problem</a><br />
+					<a class="red" href="http://help.akvo.org/discussion/new">Report a problem</a><br />
 				</p>
 			</div>
 		</div>
